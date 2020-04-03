fa8cf5fe2a76c5464836d9cbd511ca9ab7e38d17
Lakshya Bansal <bansallakshya94128@gmail.com>
2020-04-04 03:05:29 +0530

    Parsing data between home and reviewDetails

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
 
 export default function reviewDetails({navigation}) {
+    /*
     const pressHandler=()=>{
         navigation.goBack();
     }
+    */
     return (
     <View style={styles.container}>
         <Text>ReviewDetails Screen</Text>
-        <Button title="Way to home" onPress={pressHandler}/>
     </View>
     );
 }
