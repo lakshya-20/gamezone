41ffe4db63255335be9e7894d35bc08ed5097aff
Lakshya Bansal <bansallakshya94128@gmail.com>
2020-04-04 00:51:02 +0530

    Fonts keys and global styles

--- App.js
+++ App.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './screens/home'
-export default function App() {
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 
 
-  
-  return (
-    <View style={styles.container}>
-      <Home />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const getFonts = () => Font.loadAsync({
+  'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+  'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
 });
+
+export default function App() {
+  
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (fontsLoaded) {
+    return (
+      <Home />
+    );
+  } 
+  else {
+    return (
+      <AppLoading 
+        startAsync={getFonts} 
+        onFinish={() => setFontsLoaded(true)} 
+      />
+    )
+  }
+}
