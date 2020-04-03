722d46ea174373b145319ac7b35fdbb9724c026d
Lakshya Bansal <bansallakshya94128@gmail.com>
2020-04-04 03:21:40 +0530

    Drawer Navigation implemented

---
+++ routes/drawer.js
@@ -0,0 +1,17 @@
+import { createDrawerNavigator} from 'react-navigation-drawer';
+import { createAppContainer} from 'react-navigation';
+
+import HomeStack from './homeStack';
+import AboutStack from './aboutStack';
+
+const screens={
+    Home:{
+        screen:HomeStack,
+    },
+    About:{
+        screen:AboutStack,
+    }
+};
+
+const RootDrawer =createDrawerNavigator(screens);
+export default createAppContainer(RootDrawer);
