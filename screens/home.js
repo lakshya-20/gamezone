4f1bdc3f290e21d3c36dfb1800b44179208329d1
Lakshya Bansal <bansallakshya94128@gmail.com>
2020-04-05 11:55:49 +0530

    Card component

--- screens/home.js
+++ screens/home.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, Text, TouchableOpacity, FlatList } from 'react-native';
 import { globalStyles } from '../styles/global';
-
+import Card from '../shared/card';
 export default function Home({ navigation }) {
   const [reviews, setReviews] = useState([
     { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' },
@@ -13,7 +13,9 @@ export default function Home({ navigation }) {
     <View style={globalStyles.container}>
       <FlatList data={reviews} renderItem={({ item }) => (
         <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
-          <Text style={globalStyles.titleText}>{ item.title }</Text>
+          <Card>
+            <Text style={globalStyles.titleText}>{ item.title }</Text>
+          </Card>
         </TouchableOpacity>
       )} />
     </View>
--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,21 +1,34 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
-
+import { StyleSheet, View, Text, Button, Image } from 'react-native';
+import Card from '../shared/card';
+import {globalStyles, images} from '../styles/global';
 export default function reviewDetails({navigation}) {
-    /*
-    const pressHandler=()=>{
-        navigation.goBack();
-    }
-    */
-    return (
-    <View style={styles.container}>
-        <Text>ReviewDetails Screen</Text>
+    
+  const rating = navigation.getParam('rating');
+  
+  return (
+    <View style={globalStyles.container}>
+      <Card>
+        <Text style={globalStyles.titleText}>
+          { navigation.getParam('title') }
+        </Text>
+        <Text>{ navigation.getParam('body') }</Text>
+        <View style={styles.rating}>
+          <Text>GameZone rating: </Text>
+          <Image source={images.ratings[rating]} />
+        </View>
+      </Card>
     </View>
-    );
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 24,
-  },
+  rating: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingTop: 16,
+    marginTop: 16,
+    borderTopWidth: 1,
+    borderTopColor: '#eee',
+  }
 });
